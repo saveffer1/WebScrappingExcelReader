7c30ad613ed59f137a2fdd74720a4ea83df46ccf
saveffer1 <saveffer01@gmail.com>
2021-07-20 12:34:43 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SFFScrapWeb
+{
+    partial class Form1
+    {
+       
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excelbtn = new System.Windows.Forms.Button();
+            this.inputbox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rtb_debugDisplay = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // excelbtn
+            // 
+            this.excelbtn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.excelbtn.ForeColor = System.Drawing.Color.White;
+            this.excelbtn.Location = new System.Drawing.Point(15, 416);
+            this.excelbtn.Name = "excelbtn";
+            this.excelbtn.Size = new System.Drawing.Size(96, 33);
+            this.excelbtn.TabIndex = 0;
+            this.excelbtn.Text = "Excel Reader";
+            this.excelbtn.UseVisualStyleBackColor = false;
+            this.excelbtn.Click += new System.EventHandler(this.excelbtn_Click);
+            // 
+            // inputbox
+            // 
+            this.inputbox.Location = new System.Drawing.Point(50, 3);
+            this.inputbox.Name = "inputbox";
+            this.inputbox.Size = new System.Drawing.Size(285, 23);
+            this.inputbox.TabIndex = 1;
+            this.inputbox.Text = "localhost";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.inputbox, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(338, 28);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Teal;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(243, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Scraping";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtb_debugDisplay
+            // 
+            this.rtb_debugDisplay.Location = new System.Drawing.Point(12, 75);
+            this.rtb_debugDisplay.Name = "rtb_debugDisplay";
+            this.rtb_debugDisplay.Size = new System.Drawing.Size(338, 319);
+            this.rtb_debugDisplay.TabIndex = 6;
+            this.rtb_debugDisplay.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(136, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(278, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 26);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(127, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 33);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Save File";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(362, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtb_debugDisplay);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.excelbtn);
+            this.Name = "Form1";
+            this.Text = "Wiraphat & Thitiphong Web Scraper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button excelbtn;
+        private System.Windows.Forms.TextBox inputbox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtb_debugDisplay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Windows;
+
+namespace SFFScrapWeb
+{
+    public partial class Form1 : Form
+    {
+        
+
+        string keepcool;
+        public Form1()
+        {
+            InitializeComponent();
+            rtb_debugDisplay.HideSelection = false;
+        }
+
+        public async Task GetDataFromWebPage()
+        {
+            //string fullUrl = "https://coinmarketcap.com/";
+            string fullUrl = string.Format("http://{0}/old/table/stationtable.php", inputbox.Text);
+            rtb_debugDisplay.AppendText("Scraping from: " + fullUrl);
+            rtb_debugDisplay.AppendText("\n");
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(fullUrl);
+
+            ParseHtml(response);
+        }
+        public void ParseHtml(string htmlData)
+        {
+            keepcool = "";
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(htmlData);
+
+            var table = htmlDocument.DocumentNode.Descendants("tr");
+            //var data = new Dictionary<string, string>();
+            //var data2 = new Dictionary<string, string>();
+            //var data3 = new Dictionary<string, string>();
+            var csvBuilder = new StringBuilder();
+            int i = 0;
+            csvBuilder.AppendLine("Names,CPU Workload %,CPU Temp C,RAM Usage %,Last Update Date,Last Update Time");
+            foreach (var row in table)
+            {
+                i = i + 1;
+                string[] words = row.InnerText.Split(' ');
+                if (i != 1)
+                {
+                    csvBuilder.AppendLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\"", words[0], words[1], words[3], words[5], words[7], words[8]));
+                    rtb_debugDisplay.AppendText("\r" + row.InnerText);
+                }
+            }
+            string fullUrl = string.Format("http://{0}/old/table/stationtable.php", inputbox.Text);
+            rtb_debugDisplay.AppendText("Scraped from: " + fullUrl);
+            keepcool = csvBuilder.ToString().TrimStart();
+        }
+
+        private void savefile()
+        {
+            string dummyFileName = "Report_" + DateTime.Now.ToString("dd_MM_yyyy");
+
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "CSV|*.csv";
+            sf.Title = "Report";
+            sf.FileName = dummyFileName;
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                string savePath = Path.GetDirectoryName(sf.FileName);
+                rtb_debugDisplay.AppendText("\nSaved to " + savePath);
+                File.WriteAllText(sf.FileName, keepcool);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            savefile();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetDataFromWebPage();
+        }
+        private void excelbtn_Click(object sender, EventArgs e)
+        {
+            //System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            Process.Start(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\ExcelReader\Reader.exe");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtb_debugDisplay.Text = "";
+            keepcool = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /*
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("TestApp"))
+            {
+                process.Kill();
+            }
+        }
+        */
+    }
+}
